835567a77be2e3bb8f75b405219e3ac83067cdf8
mesquitafranca <mesquitafranca@yahoo.com.br>
2017-10-30 21:47:41 -0200

    Criação de classes

--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // Botão
             // 
-            this.Botão.Location = new System.Drawing.Point(337, 79);
+            this.Botão.Location = new System.Drawing.Point(103, 76);
             this.Botão.Name = "Botão";
             this.Botão.Size = new System.Drawing.Size(196, 127);
             this.Botão.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 306);
+            this.ClientSize = new System.Drawing.Size(432, 306);
             this.Controls.Add(this.Botão);
             this.Name = "Form1";
             this.Text = "Form1";
