0badc363599868ff319fb29894d4acbea657df81
mesquitafranca <mesquitafranca@yahoo.com.br>
2017-11-01 21:36:17 -0200

    G

--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(302, 107);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(391, 263);
             this.button1.TabIndex = 0;
             this.button1.Text = "butao";
             this.button1.UseVisualStyleBackColor = true;
