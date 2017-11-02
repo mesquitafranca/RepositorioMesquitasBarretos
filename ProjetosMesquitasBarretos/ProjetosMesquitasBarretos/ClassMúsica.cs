cb94135cea4ed1d913cdc0d5dc4cc1fb45ee28aa
082160008 <amorim-bruno@hotmail.com>
2017-11-01 22:15:15 -0200

    Interface em classe

--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/ClassMúsica.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/ClassMúsica.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ProjetosMesquitasBarretos
 {
-    class ClassMusica: ClasseMidia
+    class ClassMusica: ClasseMidia,ILocal
     {
+        string arquivomidia;
         int volume;
         double duracao;
         private FormatoEnumMusica fmusica;
@@ -49,6 +51,24 @@ namespace ProjetosMesquitasBarretos
             {
                 duracao = value;
             }
+        }
+
+        public string ArquivoMidia
+        {
+            get
+            {
+                return arquivomidia;
+            }
+
+            set
+            {
+                arquivomidia = value;
+            }
+        }
+
+        public bool ValidaCaminho()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/ILocal.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/ILocal.cs
@@ -8,7 +8,7 @@ namespace ProjetosMesquitasBarretos
 {
     interface ILocal
     {
-        string arquivoMidia
+        string ArquivoMidia
         {
             get;
             set;
