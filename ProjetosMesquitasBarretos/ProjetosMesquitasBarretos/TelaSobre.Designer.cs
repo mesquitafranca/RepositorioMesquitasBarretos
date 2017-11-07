cb483b8651ecd8c74415c323277eb930fb0af0dd
mesquitafranca <mesquitafranca@yahoo.com.br>
2017-11-07 00:47:55 -0200

    Tela

--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/TelaSobre.Designer.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/TelaSobre.Designer.cs
@@ -36,7 +36,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 68);
+            this.label1.Location = new System.Drawing.Point(32, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(154, 20);
             this.label1.TabIndex = 0;
