62473f7c887d5f5971608ecd976818b41321fa88
mesquitafranca <mesquitafranca@yahoo.com.br>
2017-11-08 10:31:44 -0200

    Mídia

--- ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
+++ ProjetosMesquitasBarretos/ProjetosMesquitasBarretos/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.labelNomeNaTela = new System.Windows.Forms.Label();
             this.informaçãoSobreAMídiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.panel1.SuspendLayout();
@@ -115,7 +116,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1052, 315);
+            this.label2.Location = new System.Drawing.Point(1052, 363);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 20);
             this.label2.TabIndex = 5;
@@ -125,7 +126,7 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(1051, 352);
+            this.listBox2.Location = new System.Drawing.Point(1051, 396);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(194, 124);
             this.listBox2.TabIndex = 6;
@@ -135,9 +136,9 @@
             this.labelNomeNaTela.AutoSize = true;
             this.labelNomeNaTela.Location = new System.Drawing.Point(13, 637);
             this.labelNomeNaTela.Name = "labelNomeNaTela";
-            this.labelNomeNaTela.Size = new System.Drawing.Size(114, 20);
+            this.labelNomeNaTela.Size = new System.Drawing.Size(145, 20);
             this.labelNomeNaTela.TabIndex = 7;
-            this.labelNomeNaTela.Text = "Nome da Mídia";
+            this.labelNomeNaTela.Text = "Selecione a Mídia...";
             // 
             // informaçãoSobreAMídiaToolStripMenuItem
             // 
@@ -146,11 +147,21 @@
             this.informaçãoSobreAMídiaToolStripMenuItem.Text = "Informações sobre a Mídia";
             this.informaçãoSobreAMídiaToolStripMenuItem.Click += new System.EventHandler(this.informaçãoSobreAMídiaToolStripMenuItem_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1112, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "V";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Tela1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelNomeNaTela);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.label2);
@@ -190,6 +201,7 @@
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Label labelNomeNaTela;
         private System.Windows.Forms.ToolStripMenuItem informaçãoSobreAMídiaToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
